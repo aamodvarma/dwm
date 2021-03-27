cc8e7cf9afb7129c20595025f892819742617153
aamodvarma <aamodvarma@gmail.com>
2021-03-27 09:58:17 +0400

    idk what changes

--- config.h
+++ config.h
@@ -1,35 +1,56 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
+static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const unsigned int baralpha = 0xd0;
 
 
-static const char *fonts[]     = {"Mononoki Nerd Font:size=9:antialias=true:autohint=true",
+static const int vertpad                 = 10;  /* vertical padding of bar */
+static const int sidepad                 = 20;  /* horizontal padding of bar */
+
+/*static const char *fonts[]     = {"Mononoki Nerd Font:size=9:antialias=true:autohint=true",
                                   "Hack:size=8:antialias=true:autohint=true",
                                   "JoyPixels:size=10:antialias=true:autohint=true"};
-static const char col_1[]  = "#282c34"; /* background color of bar */
+				  */
+static const char *fonts[]          = { "CaskaydiaCove Nerd Font:size=10" };
+
+static const char col_1[]  = "#0f111a"; /* background color of bar */
 static const char col_2[]  = "#282c34"; /* border color unfocused windows */
 static const char col_3[]  = "#d7d7d7";
-static const char col_4[]  = "#924441"; /* border color focused windows and tags */
-
-
+static const char col_4[]  = "#4b0082"; /* border color focused windows and tags */
 static const char *colors[][3]      = {
+
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_3, col_1, col_2 },
 	[SchemeSel]  = { col_3, col_4,  col_4  },
 };
 
-/* tagging */
 
-static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* tagging */
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+//static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
@@ -43,7 +64,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 
 #define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
